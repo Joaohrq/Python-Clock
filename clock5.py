9876c03b2c5b1060ce7e8d39ffc415b6dd056bb1
Joaohrq <131315877+Joaohrq@users.noreply.github.com>
2025-08-24 15:53:11 -0300

    clock5.py
    idk whats the difference honestly

---
+++ clock5.py
@@ -0,0 +1,61 @@
+import time
+
+def minuto_ou_hora():
+    try:
+        tipo_tempo = int(input("Escolha: 1- Segundos. 2- Minutos: "))
+        if tipo_tempo == 1:
+            return 1
+        if tipo_tempo == 2:
+            return 2
+        else:
+            print("Opção inválida.")
+            quit()
+    except:
+        print("digito inválido.")
+        quit()
+
+def tempo_correr(a):
+    try:
+        if a == 1:
+            segundos = int(input("Quantos segundos? "))
+            return segundos
+        if a == 2:
+            minutos = float(input("Quantos minutos? "))
+            return minutos
+    except:
+        print("Dígito invalido.")
+        quit()
+
+def segundos_passados(b):
+    c = b
+    while c > 0:
+        print(c)
+        time.sleep(1)
+        c -= 1
+    if b > 1:
+        print(f"Contagem acabou. Passaram-se {b} segundos.")
+    elif b == 1:
+        print("Contagem acabou, passou 1 segundo.")
+
+def minutos_passados(d):
+    f = d * 60
+    e = f
+    while e > 0:
+        print(e)
+        time.sleep(1)
+        e -= 1
+    if d > 1:
+        print(f"Contagem acabou. Passaram-se {d} minutos.")
+    elif d == 1:
+        print("Contagem acabou, passou 1 minuto.")
+
+
+min_ou_hora = minuto_ou_hora()
+
+if min_ou_hora == 1:
+    tempo_a_correr = tempo_correr(min_ou_hora)
+    segundos_passados(tempo_a_correr)
+
+if min_ou_hora == 2:
+    tempo_a_correr = tempo_correr(min_ou_hora)
+    minutos_passados(tempo_a_correr)
